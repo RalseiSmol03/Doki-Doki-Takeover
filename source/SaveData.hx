96f61dfd31d10f0d1e0aa45f2912b82909d878e0
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-14 22:34:10 +0200

    Update SaveData.hx

--- source/SaveData.hx
+++ source/SaveData.hx
@@ -39,7 +39,7 @@ class SaveData
 	public static var gfCountdown:Null<Bool> = false;
 	public static var zoom:Null<Float> = 1;
 	public static var middleOpponent:Null<Bool> = false;
-	public static var gpuTextures:Null<Bool> = true;
+	public static var gpuTextures:Null<Bool> = #if mobile false #else true #end;
 	public static var cacheCharacter:Null<Bool> = false;
 	public static var cacheSong:Null<Bool> = false;
 	public static var middleScroll:Null<Bool> = false;
